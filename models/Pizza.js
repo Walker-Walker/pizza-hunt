c85c11001379f28374a2dfcf01d3f17dddeaa1d6
Walker-Walker <awalker0@gmail.com>
2020-09-15 17:08:06 -0700

    completed CRUD for user comments on pizzas

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,26 +1,49 @@
+
 const { Schema, model } = require("mongoose");
+const moment = require('moment');
 // importing the Schema construtor from mongoose dependency and the model function
 //Schema for the data
 
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String,
-  },
-  createdBy: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  size: {
-    type: String,
-    default: "Large",
-  },
-  toppings: [],
-});
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+    },
+    createdBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    size: {
+      type: String,
+      default: "Large",
+    },
+    toppings: [],
 
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false,
+  }
+);
+
+//get total count of comments and replies on retrieval
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
+});
 // create the Pizza model using the PizzaSchema
-const Pizza = model('Pizza', PizzaSchema);
+const Pizza = model("Pizza", PizzaSchema);
 //export the Pizza model
 module.exports = Pizza;
