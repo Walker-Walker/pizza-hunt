2869e4ef0433314bff773c08110d234fb9a3b4ca
Walker-Walker <awalker0@gmail.com>
2020-09-17 17:55:40 -0700

    added PWA close to functionality using IndexedDB

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -63,6 +63,7 @@ const handlePizzaSubmit = (event) => {
     })
     .catch(err => {
       console.log(err);
+      saveRecord(formData);//save pizza w/out internet connection to new_pizza object store in browsers IndexedDB database : pizza_hunt
     });
 };
 
